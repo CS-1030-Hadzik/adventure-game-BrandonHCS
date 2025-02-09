253a63b5b6b6d0a2f2d6c4e3062e8433d48a519c
hrng <brandonheywood@mail.weber.edu>
2025-02-09 14:44:42 -0700

    Attempting to make a library, not sure if I need to commit it to make the file structure function, so this is a test.

--- Adventure Game/game.py
+++ Adventure Game/game.py
@@ -6,7 +6,10 @@ project (if I remember correctly) I'll need to have at least three
 different choices with multiple options for each
 """
 
+import gamefuns
 #pull the player's name. This will be how the system refers to them for the rest of the game.
 PlayerName = input("How would you like me to refer to you, user? \n")
 #Welcome them to the game, edit to fit the tone of the game later if needed
 print(f"Welcome to the adventure game {PlayerName}!\nYour adventure will begin shortly.")
+
+IntroductoryScript(PlayerName)
---
+++ Adventure Game/gamefuns.py
@@ -0,0 +1,5 @@
+
+
+def IntroductoryScript(PlayerName):
+    print(f"\nYou find yourself waking up with a pounding headache. A warm orange light glows on the other side of the room as you. \"Are you okay, {PlayerName}?\" You try to respond, but all you can manage in this moment is a slight groan. \"You took a pretty heavy blow to the head and were out for a while. Luckily I found us some cover in this cave, so we should be safe for now.\" Your companion's voice grows closer and clearer as your headache lessens slightly, sounding deep and gravelly. \"We should probably stay here for a few more hours at least.\"\n\nDo you stay?")
+    return input("input the # indicated between the '#' to respond\n'1' Stay here with your companion\n'2' Try your luck on the road\n'3' Ask some questions about where you are and what you were doing\n")
